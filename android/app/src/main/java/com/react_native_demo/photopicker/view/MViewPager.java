3d4b48f00ffd05070006ea7cc40cbe5958bad88f
wenqiang <717092390@qq.com>
2017-11-18 10:37:15 +0800

    解决photoView缩放闪退的问题

---
+++ android/app/src/main/java/com/react_native_demo/photopicker/view/MViewPager.java
@@ -0,0 +1,39 @@
+package com.react_native_demo.photopicker.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by wenqiang on 2017/11/18.
+ */
+
+public class MViewPager extends ViewPager {
+    public MViewPager(Context context) {
+        super(context);
+    }
+
+    public MViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (Exception ex) {
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (Exception e) {
+        }
+        return false;
+    }
+}
+
